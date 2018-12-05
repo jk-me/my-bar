2e909de1f400d83be4bfcf2388a564eda7928f9c
Jenny Kam <jkam17@gmail.com>
2018-12-05 18:20:42 -0500

    add controller methods to users and drinks

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,20 @@
 class DrinksController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
