9358acd67e1bf9bc8dfe1e9e28b46ced958a2484
Jenny Kam <jkam17@gmail.com>
2018-12-05 21:36:57 -0500

    add new, create, show controller method and views for user and private method for user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
   end
 
   def show
+    @user = User.find(session[:user_id])
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
 end
