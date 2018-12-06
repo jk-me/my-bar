020065032b3270374f4376557d6e50769bce0b14
Jenny Kam <jkam17@gmail.com>
2018-12-06 17:01:13 -0500

    fill out create method for drinks controller, add drinks index and view

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,10 +1,16 @@
 class DrinksController < ApplicationController
 
+  def index
+    @drinks = Drink.all
+  end
+  
   def new
     @drink = Drink.new
   end
 
   def create
+    @drink = Drink.create(drink_params)
+    redirect_to drink_path(@drink)
   end
 
   def show
@@ -18,5 +24,11 @@ class DrinksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def drink_params
+    params.require(:drink).permit(:name, :description)
   end
 end
