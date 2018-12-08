c00220d0e5a1ad6e8fcfece36d3b8e2e7e0fbb2e
Jenny Kam <jkam17@gmail.com>
2018-12-08 00:16:14 -0500

    draft new review view, controller methods, not working yet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,17 @@
 class ReviewsController < ApplicationController
   before_action :redirect_if_not_logged_in
-  
+
   def new
     @review = Review.new
   end
 
   def create
+    @review = Review.create(review_params)
+    if @review.valid?
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +21,11 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
