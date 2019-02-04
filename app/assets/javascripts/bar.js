9cd1739b707e0362e346b18ccb62fc68687804ff
Jenny Kam <jkam17@gmail.com>
2019-02-04 13:41:16 -0500

    remove turbolinks, add see less link to reviews to toggle more or less content

--- app/assets/javascripts/bar.js
+++ app/assets/javascripts/bar.js
@@ -1,14 +1,36 @@
 $(function(){
   console.log('bar.js loaded')
-  listener()
+  fullReview()
 })
 
-function listener(){
+function fullReview(){
   $('.full-rev').on('click', function(e){
     e.preventDefault()
+    // if (e.preventDefault) {console.log('prevented')}
+
     let id = $(this).data('id')
-    $.get('/reviews/'+ id +'.json', function(data){
+    // debugger
+    $.get('/reviews/'+ id , function(data){
       $(`#review-cell-`+ id).text(data.content)
+      $(`#review-cell-`+ id).append(
+      `<a class="short-rev" data-id=${id} href="#">See Less</a>`)
+      miniReview()
+    })
+  })
+}
+
+function miniReview(){
+  // debugger
+  $('.short-rev').on('click', function(e){
+    e.preventDefault()
+    // if (e.preventDefault) {console.log('prevented')}
+    let id = $(this).data('id')
+    $.get('/reviews/'+ id , function(data){
+      $(`#review-cell-`+ id).text(data.content.substring(0,31))
+      $(`#review-cell-`+ id).append(
+      `<a class="full-rev" data-id=${id} href="#">See More</a>`)
+      fullReview()
+
     })
   })
 }
