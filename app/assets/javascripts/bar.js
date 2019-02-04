76e6d9a1b9d12b3171dba88599e4804b7f68ea15
Jenny Kam <jkam17@gmail.com>
2019-02-04 13:46:14 -0500

    clean up review listeners js code, remove "see more" links for short reviews

--- app/assets/javascripts/bar.js
+++ app/assets/javascripts/bar.js
@@ -6,31 +6,25 @@ $(function(){
 function fullReview(){
   $('.full-rev').on('click', function(e){
     e.preventDefault()
-    // if (e.preventDefault) {console.log('prevented')}
-
     let id = $(this).data('id')
-    // debugger
     $.get('/reviews/'+ id , function(data){
       $(`#review-cell-`+ id).text(data.content)
       $(`#review-cell-`+ id).append(
-      `<a class="short-rev" data-id=${id} href="#">See Less</a>`)
+      ` <a class="short-rev" data-id=${id} href="#">See Less</a>`)
       miniReview()
     })
   })
 }
 
 function miniReview(){
-  // debugger
   $('.short-rev').on('click', function(e){
     e.preventDefault()
-    // if (e.preventDefault) {console.log('prevented')}
     let id = $(this).data('id')
     $.get('/reviews/'+ id , function(data){
-      $(`#review-cell-`+ id).text(data.content.substring(0,31))
+      $(`#review-cell-`+ id).text(data.content.substring(0,27) + '...')
       $(`#review-cell-`+ id).append(
-      `<a class="full-rev" data-id=${id} href="#">See More</a>`)
+      ` <a class="full-rev" data-id=${id} href="#">See More</a>`)
       fullReview()
-
     })
   })
 }
