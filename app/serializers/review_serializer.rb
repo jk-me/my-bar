ce8684575ee8fc1b2c7450e710866e41061bd12f
Jenny Kam <jkam17@gmail.com>
2019-02-05 17:02:37 -0500

    add user name and id to review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :rating, :content
+  attributes :id, :rating, :content, :user_id, :user_name
 
   belongs_to :user
   belongs_to :drink
