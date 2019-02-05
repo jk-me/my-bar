3c72041c3399e81f6b587ba3d0a9251490721436
Jenny Kam <jkam17@gmail.com>
2019-02-05 13:48:47 -0500

    add ingredient serializer, render ingredient json in show view, add user/id/drink and ingredient/id/drink show views

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :reviews, only:[:index]
+    resources :drinks, only:[:show]
   end
   resources :drinks do
     resources :reviews, only: [:new]
